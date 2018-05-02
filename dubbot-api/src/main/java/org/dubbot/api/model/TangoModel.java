ee010b43ca8d8d70f66e63fc7bc173ddca29a645
heijing <heijing@pinduoduo.com>
2018-05-02 13:35:10 +0800

    add modle

---
+++ dubbot-api/src/main/java/org/dubbot/api/model/TangoModel.java
@@ -0,0 +1,31 @@
+package org.dubbot.api.model;
+
+import java.io.Serializable;
+
+public class TangoModel implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Integer shipId;
+    private Long mallId;
+    private Boolean flag;
+    
+    public Integer getShipId() {
+        return shipId;
+    }
+    public void setShipId(Integer shipId) {
+        this.shipId = shipId;
+    }
+    public Long getMallId() {
+        return mallId;
+    }
+    public void setMallId(Long mallId) {
+        this.mallId = mallId;
+    }
+    public Boolean getFlag() {
+        return flag;
+    }
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+}
